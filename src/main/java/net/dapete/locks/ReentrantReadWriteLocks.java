67dea031642c6f2c5ad6213ad52b167d833e0ae5
Peter Schlömer (dapete) <dev@dapete.net>
2025-02-14 17:40:25 +0100

    Add missing Javadoc comments

--- src/main/java/net/dapete/locks/ReentrantReadWriteLocks.java
+++ src/main/java/net/dapete/locks/ReentrantReadWriteLocks.java
@@ -2,6 +2,13 @@ package net.dapete.locks;
 
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+/**
+ * Key-based locking with {@link ReentrantReadWriteLock}s.
+ * <p>
+ * Instances can be created using {@link ReadWriteLocks#reentrant()} and {@link ReadWriteLocks#reentrant(Class)}.
+ *
+ * @param <K> type of key
+ */
 public class ReentrantReadWriteLocks<K> extends ReadWriteLocks<K, ReentrantReadWriteLock> {
 
     ReentrantReadWriteLocks() {
