35f6fd09eb3210b1f68f559997cf7df709291945
Peter Schlömer (dapete) <dev@dapete.net>
2025-02-20 21:40:09 +0100

    Add module-info.java; version bump to 1.1.0

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+import org.jspecify.annotations.NullMarked;
+
+/**
+ * <p>
+ * This module contains classes for key-based locking, as in a way to obtain {@link java.util.concurrent.locks.Lock Lock}s
+ * or {@link java.util.concurrent.locks.ReadWriteLock ReadWriteLock}s which are identified by a key.
+ * </p>
+ *
+ * @see net.dapete.locks documentation for package net.dapete.locks
+ * @since 1.1.0
+ */
+@NullMarked
+module net.dapete.locks {
+    requires java.base;
+    requires org.jspecify;
+    exports net.dapete.locks;
+}
