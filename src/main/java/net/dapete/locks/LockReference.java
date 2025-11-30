b5fa01142488ab04ba173837e69aa87c9bde7e60
Peter Schlömer (dapete) <dev@dapete.net>
2025-11-30 12:53:11 +0100

    Make keys nullable

--- src/main/java/net/dapete/locks/LockReference.java
+++ src/main/java/net/dapete/locks/LockReference.java
@@ -1,18 +1,20 @@
 package net.dapete.locks;
 
+import org.jspecify.annotations.Nullable;
+
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
 
-final class LockReference<K, L> extends WeakReference<L> {
+final class LockReference<K extends @Nullable Object, L> extends WeakReference<L> {
 
-    private final K key;
+    private final @Nullable K key;
 
-    LockReference(K key, L value, ReferenceQueue<? super L> referenceQueue) {
+    LockReference(@Nullable K key, L value, ReferenceQueue<? super L> referenceQueue) {
         super(value, referenceQueue);
         this.key = key;
     }
 
-    K getKey() {
+    @Nullable K getKey() {
         return key;
     }
 
--- src/main/java/net/dapete/locks/LocksImpl.java
+++ src/main/java/net/dapete/locks/LocksImpl.java
@@ -1,16 +1,18 @@
 package net.dapete.locks;
 
+import org.jspecify.annotations.Nullable;
+
 import java.util.concurrent.locks.Lock;
 import java.util.function.Supplier;
 
-class LocksImpl<K, L extends Lock> extends AbstractLocks<K, L> implements Locks<K, L> {
+class LocksImpl<K extends @Nullable Object, L extends Lock> extends AbstractLocks<K, L> implements Locks<K, L> {
 
     LocksImpl(Supplier<L> lockSupplier) {
         super(lockSupplier);
     }
 
     @Override
-    public final L lock(K key) {
+    public final L lock(@Nullable K key) {
         final var lock = get(key);
         lock.lock();
         return lock;
